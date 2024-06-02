3177e3f03179c524e40d2dedce77213a7d7570b4
Michael Siega <mf.siega@gmail.com>
2024-06-02 03:08:48 -0400

    move board to its own component

--- src/App.js
+++ src/App.js
@@ -12,60 +12,7 @@ import {
   RED_CHOSE_PIECE,
 } from "./common/constants";
 import { MoveValidator } from "./components/moveValidator";
-
-const getClassName = (row, column, value) => {
-  const isLight = (row + column) % 2 === 0;
-  const isHighlighted =
-    value === BLACK_PIECE_HIGHLIGHTED || value === RED_PIECE_HIGHLIGHTED;
-  const hasBlackPiece =
-    value === BLACK_PIECE || value === BLACK_PIECE_HIGHLIGHTED;
-  const hasRedPiece = value === RED_PIECE || value === RED_PIECE_HIGHLIGHTED;
-  let squareColor = "";
-  if (isLight) {
-    squareColor = "light";
-  } else if (isHighlighted) {
-    squareColor = "darkhighlighted";
-  } else {
-    squareColor = "dark";
-  }
-  let piece = "";
-  if (hasBlackPiece) {
-    piece = "withblackpiece";
-  }
-  if (hasRedPiece) {
-    piece = "withredpiece";
-  }
-  const c = `square ${squareColor} ${piece}`;
-  return c;
-};
-
-function Square({ row, column, value, onSquareClick }) {
-  return (
-    <button
-      className={getClassName(row, column, value)}
-      onClick={onSquareClick}
-    ></button>
-  );
-}
-
-function Board({ gamePhase, squares, onPlay }) {
-  console.log(squares);
-  return (
-    <>
-      <div className="board">
-        {squares.map((_, index) => (
-          <Square
-            key={index}
-            row={Math.floor(index / 8)}
-            column={index % 8}
-            value={squares[index]}
-            onSquareClick={() => onPlay(gamePhase, squares, index)}
-          />
-        ))}
-      </div>
-    </>
-  );
-}
+import {Board} from "./components/Board";
 
 function App() {
   // Initialization.
